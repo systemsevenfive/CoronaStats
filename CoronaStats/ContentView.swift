7ec3c85c6362205f209c457b8432ca50e7a02799
systemsevenfive <r.saunders@icloud.com>
2020-07-04 22:02:11 -0400

    Initial Commit

---
+++ CoronaStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaStats
+//
+//  Created by Ryan Saunders on 2020-07-04.
+//  Copyright © 2020 Ryan Saunders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
